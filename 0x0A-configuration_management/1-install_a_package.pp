b51982bae535bab3035f95392ad4447616dc45c2
BinyamMamo <binyammamo01@gmail.com>
2023-07-01 10:30:48 +0300

    Sat Jul  1 10:30:48 EAT 2023

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,34 +3,6 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-package { 'python3-setuptools':
-  ensure => installed,
-}
-
-package { 'python3-wheel':
-  ensure => installed,
-}
-
-package { 'python3-dev':
-  ensure => installed,
-}
-
-package { 'build-essential':
-  ensure => installed,
-}
-
-package { 'libssl-dev':
-  ensure => installed,
-}
-
-package { 'libffi-dev':
-  ensure => installed,
-}
-
-package { 'python3-cffi':
-  ensure => installed,
-}
-
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
