c033335a2ee23742942ff272f8e28a9b6b9671de
BinyamMamo <binyammamo01@gmail.com>
2023-07-01 10:42:58 +0300

    Sat Jul  1 10:42:58 EAT 2023

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,10 @@
 # terminates a program after executing a command from puppet
+package { 'pgrep':
+  ensure => installed,
+}
+package { 'pkill':
+  ensure => installed,
+}
 exec { 'kill_me_now':
   command => ['/usr/bin/pkill', 'killmenow'],
   onlyif  => 'pgrep killmenow',
