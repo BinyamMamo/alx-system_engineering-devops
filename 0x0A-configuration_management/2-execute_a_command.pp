4a436d600ea8985f497bcdf2c0696c1e06a068c9
BinyamMamo <binyammamo01@gmail.com>
2023-07-01 10:52:20 +0300

    Sat Jul  1 10:52:20 EAT 2023

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -7,6 +7,6 @@ package { 'pkill':
 }
 exec { 'killmenow':
   command => 'pkill -f killmenow',
-  path => '/usr/bin/:/usr/local/bin/:/bin/',
+  path    => '/usr/bin/:/usr/local/bin/:/bin/',
   onlyif  => 'pgrep killmenow',
 }
