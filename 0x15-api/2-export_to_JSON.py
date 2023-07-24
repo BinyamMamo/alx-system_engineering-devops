b882a44e0a5c7d7fbe3b72dee33e4b99e1c283c0
BinyamMamo <binyammamo01@gmail.com>
2023-07-24 14:39:14 +0300

    Mon Jul 24 14:39:14 EAT 2023

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,11 +6,6 @@ import requests
 from sys import argv
 import json
 
-
-# user id received from the argument
-uid = argv[1]
-
-
 def extract(end_point):
     """return the response object from the GET request
         made to the specified endpoint
@@ -19,15 +14,19 @@ def extract(end_point):
     return requests.get(base_uri + end_point).json()
 
 
-todos = extract(f"user/{uid}/todos")
-todo_list = []
-for task in todos:
-    todo = {
-        "task": task['title'],
-        "completed": task['completed'],
-        "username": extract(f"users/{uid}")['username']
-    }
-    todo_list.append(todo)
+if __name__ == "main":
+    # user id received from the argument
+    uid = argv[1]
 
-with open(f"{uid}.json", 'w', newline='') as f:
-    json.dump({'2': todo_list}, f)
+    todos = extract(f"user/{uid}/todos")
+    todo_list = []
+    for task in todos:
+        todo = {
+            "task": task['title'],
+            "completed": task['completed'],
+            "username": extract(f"users/{uid}")['username']
+        }
+        todo_list.append(todo)
+
+    with open(f"{uid}.json", 'w', newline='') as f:
+        json.dump({'2': todo_list}, f)
