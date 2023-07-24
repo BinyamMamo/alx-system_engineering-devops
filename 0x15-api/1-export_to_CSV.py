1439ae27ea68ab3e0051b6224b9ea9e406579e7b
BinyamMamo <binyammamo01@gmail.com>
2023-07-24 14:38:17 +0300

    Mon Jul 24 14:38:17 EAT 2023

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,24 +3,22 @@
 Extracts a user's to-do list from a JSON API and saves it as a CSV file.
 """
 
-
 import requests
 from sys import argv
 from csv import DictWriter, QUOTE_ALL
 
 
+def extract(end_point):
+    """return the response object from the GET request
+        made to the specified endpoint
+    """
+    base_uri = "https://jsonplaceholder.typicode.com/"
+    return requests.get(base_uri + end_point).json()
+
+
 if __name__ == "__main__":
     # user id received from the argument
     uid = argv[1]
-
-
-    def extract(end_point):
-        """return the response object from the GET request
-            made to the specified endpoint
-        """
-        base_uri = "https://jsonplaceholder.typicode.com/"
-        return requests.get(base_uri + end_point).json()
-
 
     todos = extract(f"user/{uid}/todos")
     todo_list = []
