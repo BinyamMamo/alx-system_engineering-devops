110a3c762d61602c81e11309ab615eb2885d0737
BinyamMamo <binyammamo01@gmail.com>
2023-11-24 20:52:18 +0400

    Fri Nov 24 20:52:18 +04 2023

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,7 @@
-# installs the flask package(version 2.1.0) using puppet
-package { 'python3-pip':
-  ensure   => installed,
-}
+#!/usr/bin/pup
+# install flask
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package {'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3'
 }
