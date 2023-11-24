ec9f54db994b7b8906a607ab9b720527262e7ecc
BinyamMamo <binyammamo01@gmail.com>
2023-11-24 19:32:12 +0400

    Fri Nov 24 19:32:12 +04 2023

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,11 @@
 # install flask package if not found
 
+# installs required package
 package { 'python3-pip':
   ensure => installed,
 }
 
+# installs the flask package
 exec { 'install_flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
   path    => '/usr/bin',
