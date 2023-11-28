e27b5433f33c6ccdf596e68baa11e582abb902ce
BinyamMamo <binyammamo01@gmail.com>
2023-11-28 22:00:34 +0400

    Tue Nov 28 22:00:34 +04 2023

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,59 +3,68 @@
 exec { 'apt-update':
   command     => 'apt-get update',
   path        => '/usr/bin',
-  refreshonly => true
+  refreshonly => true,
+  privileged  => true
 }
 
 package { 'nginx':
-  ensure => installed
+  ensure     => installed,
+  privileged => true
 }
 
 class { 'ufw':
-  allow  => ['ssh', 'http', 'Nginx HTTP'],
+  allow      => ['ssh', 'http', 'Nginx HTTP'],
+  privileged => true
 }
 
 file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World'
+  ensure     => file,
+  content    => 'Hello World',
+  privileged => true
 }
 
 file { '/etc/nginx/sites-available/redirect':
-  ensure  => file,
-  content => "server {
+  ensure     => file,
+  content    => "server {
                 listen 80;
                 server_name _;
                 location /redirect_me {
                   return 301 /;
                 }
-              }"
+              }",
+  privileged => true
 }
 
 file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "server {
+  ensure     => file,
+  content    => "server {
                 listen 80;
                 server_name _;
                 location / {
                   root /var/www/html;
                   index index.html;
                 }
-              }"
+              }",
+  privileged => true
 }
 
 file { '/etc/nginx/sites-enabled/redirect':
-  ensure  => link,
-  target  => '/etc/nginx/sites-available/redirect',
-  require => File['/etc/nginx/sites-available/redirect']
+  ensure     => link,
+  target     => '/etc/nginx/sites-available/redirect',
+  require    => File['/etc/nginx/sites-available/redirect'],
+  privileged => true
 }
 
 file { '/etc/nginx/sites-enabled/default':
-  ensure  => link,
-  target  => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default']
+  ensure     => link,
+  target     => '/etc/nginx/sites-available/default',
+  require    => File['/etc/nginx/sites-available/default'],
+  privileged => true
 }
 
 service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => [File['/etc/nginx/sites-enabled/redirect'], File['/etc/nginx/sites-enabled/default']]
+  ensure     => running,
+  enable     => true,
+  require    => [File['/etc/nginx/sites-enabled/redirect'], File['/etc/nginx/sites-enabled/default']],
+  privileged => true
 }
